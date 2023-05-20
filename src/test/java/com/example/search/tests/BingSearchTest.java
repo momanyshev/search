c87190ed594bbe53bdb72d4c3c1eea563427b442
momanyshev <maksimmanyshev@yandex.ru>
2023-05-20 13:40:44 +0300

    added pattern PageObject

--- src/test/java/com/example/search/tests/BingSearchTest.java
+++ src/test/java/com/example/search/tests/BingSearchTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,7 +15,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.List;
 
 public class BingSearchTest {
     private WebDriver driver;
